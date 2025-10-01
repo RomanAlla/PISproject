0a8c441fb4f1252179720f06aae206aeb097de03
Роман Алла <romanalla@MacBook-Air-Roman.local>
2025-10-01 16:26:13 +0700

    ввел мелкие изменения

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,8 @@
 import 'services/file_proccesor.dart';
-import 'services/point_parser.dart';
-import 'utils/distance_calculator.dart';
-import 'utils/exceptions.dart';
-import 'utils/output_printer.dart';
 
 const String dataPath = 'input_data.txt';
 
 void main() {
-  try {
-    final lines = FileProccesor.readDataFromFile(dataPath);
-    final fileContent = lines.join('\n');
-    final points = PointParser().parsePointsFromString(fileContent);
-
-    OutputPrinter.printPointsInfo(points);
-    OutputPrinter.printClosestPointInfo(
-      DistanceCalculator.findClosestToOrigin(points),
-    );
-  } on ParseException catch (e) {
-    print('Ошибка парсинга из файла: $e');
-  } catch (e) {
-    print('Ошибка: $e');
-  }
-
-  print('\n=== ОБРАБОТКА ИЗ СТРОКИ ===');
-  const testData = '''
-   colored_point 1.0 2.0 red
-   weighted_point 3.0 4.0 green 5.5
-   timed_point 5.0 6.0 blue 2023-01-01 12:00:00
-   colored_point 0.5 0.5 red
-   invalid 10.0 20.0 yellow
-   ''';
-
-  try {
-    final stringPoints = PointParser().parsePointsFromString(testData);
-    OutputPrinter.printPointsInfo(stringPoints);
-    OutputPrinter.printClosestPointInfo(
-      DistanceCalculator.findClosestToOrigin(stringPoints),
-    );
-  } on ParseException catch (e) {
-    print('Ошибка парсинга из строки: $e');
-  }
+  FileProccesor.processFileData(dataPath);
+  FileProccesor.processStringData();
 }
--- lib/services/file_proccesor.dart
+++ lib/services/file_proccesor.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+import 'package:PISprojects/services/point_parser.dart';
+import 'package:PISprojects/utils/distance_calculator.dart';
+import 'package:PISprojects/utils/output_printer.dart';
+
 import '../utils/exceptions.dart';
 
 class FileProccesor {
@@ -12,6 +16,46 @@ class FileProccesor {
       return file.readAsLinesSync();
     } catch (e) {
       throw ParseException('Ошибка при чтении файла "$dataPath": $e');
+    }
+  }
+
+  static void processFileData(String dataPath) {
+    try {
+      final lines = FileProccesor.readDataFromFile(dataPath);
+      final fileContent = lines.join('\n');
+      final points = PointParser().parsePointsFromString(fileContent);
+
+      OutputPrinter.printPointsInfo(points);
+      OutputPrinter.printClosestPointInfo(
+        DistanceCalculator.findClosestToOrigin(points),
+      );
+    } on ParseException catch (e) {
+      print('Ошибка парсинга из файла: $e');
+    } catch (e) {
+      print('Ошибка: $e');
+    }
+  }
+
+  static void processStringData() {
+    print('\n=== ОБРАБОТКА ИЗ СТРОКИ ===');
+    const testData = '''colored_point 1.0 2.0 red 
+weighted_point 3.0 4.0 green 5.5
+timed_point 5.0 6.0 blue 2023-01-01 12:00:00
+colored_point 0.5 0.5 red
+colored_point 2.0 3.0 invalid_color
+invalid_data
+line 4.2 3.2''';
+
+    try {
+      final stringPoints = PointParser().parsePointsFromString(testData);
+      OutputPrinter.printPointsInfo(stringPoints);
+      OutputPrinter.printClosestPointInfo(
+        DistanceCalculator.findClosestToOrigin(stringPoints),
+      );
+    } on ParseException catch (e) {
+      print('Ошибка парсинга из строки: $e');
+    } catch (e) {
+      print(e);
     }
   }
 }
